172cb2f2fa03dc756445126c669e91f000399094
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-04-19 18:41:24 +0300

    use denyWithStatus response method to return 404 instead 403

--- app/Policies/EntryPolicy.php
+++ app/Policies/EntryPolicy.php
@@ -4,14 +4,17 @@ namespace App\Policies;
 
 use App\Models\Entry;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class EntryPolicy
 {
     /**
      * Determine whether the user can work with Entry model.
      */
-    public function workWith(?User $user, Entry $entry): bool
+    public function workWith(?User $user, Entry $entry): Response
     {
-        return $user?->id === $entry->user_id;
+        return $user?->id === $entry->user_id
+            ? Response::allow()
+            : Response::denyWithStatus(404);
     }
 }
