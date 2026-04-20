1b6be259718b5b16aaa77d27901d4a0aa7bbf88e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-04-20 12:04:48 +0300

    show entry page: move buttons on top

--- resources/views/components/entries/⚡show.blade.php
+++ resources/views/components/entries/⚡show.blade.php
@@ -36,14 +36,14 @@ new class extends Component {
 ?>
 
 <div class="flex max-w-3xl mx-auto flex-1 flex-col gap-4 pt-6 rounded-xl">
-
-    {{-- HEADER --}}
-    <div class="flex justify-between items-center">
-        <h1 class="text-2xl font-bold">
-            {{ $entry->title ?: 'No title' }}
-        </h1>
-
-        <div class="flex gap-2">
+    {{-- HEADER ACTIONS --}}
+    <div class="flex items-center justify-between">
+        {{-- Back --}}
+        <flux:button variant="filled" icon="arrow-uturn-left" wire:navigate href="{{ route('entries.index') }}">
+            back to all entries
+        </flux:button>
+        {{-- Actions --}}
+        <div class="flex flex-wrap gap-2">
             <flux:button wire:navigate href="{{ route('entries.edit', $entry) }}" variant="primary">
                 Edit
             </flux:button>
@@ -51,49 +51,49 @@ new class extends Component {
             <flux:modal.trigger name="delete-entry">
                 <flux:button variant="danger">Delete</flux:button>
             </flux:modal.trigger>
-
-            <flux:modal name="delete-entry" class="min-w-[22rem]">
-                <div class="space-y-6">
-                    <div>
-                        <flux:heading size="lg">Delete entry?</flux:heading>
-
-                        <flux:text class="mt-2">
-                            You're about to delete this entry.<br>
-                            This action cannot be reversed.
-                        </flux:text>
-                    </div>
-
-                    <div class="flex gap-2">
-                        <flux:spacer />
-
-                        <flux:modal.close>
-                            <flux:button variant="ghost">Cancel</flux:button>
-                        </flux:modal.close>
-
-                        <form method="POST" wire:submit.prevent="delete">
-                            <flux:button type="submit" variant="danger">Delete</flux:button>
-                        </form>
-                    </div>
-                </div>
-            </flux:modal>
         </div>
     </div>
+    {{-- DATE --}}
+    <div class="text-sm text-gray-400">
+        {{ $entry->created_at->format('d M Y H:i') }}
+    </div>
+    {{-- TITLE --}}
+    <h1 class="text-2xl font-bold wrap-break-words">
+        {{ $entry->title ?: 'No title' }}
+    </h1>
+    {{-- MODAL --}}
+    <flux:modal name="delete-entry" class="min-w-[22rem]">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Delete entry?</flux:heading>
 
+                <flux:text class="mt-2">
+                    You're about to delete this entry.<br>
+                    This action cannot be reversed.
+                </flux:text>
+            </div>
+
+            <div class="flex gap-2">
+                <flux:spacer />
+
+                <flux:modal.close>
+                    <flux:button variant="ghost">Cancel</flux:button>
+                </flux:modal.close>
+
+                <form method="POST" wire:submit.prevent="delete">
+                    <flux:button type="submit" variant="danger">Delete</flux:button>
+                </form>
+            </div>
+        </div>
+    </flux:modal>
     {{-- MOOD --}}
     @if($entry->mood)
         <div class="text-sm text-gray-500">
             {{ $entry->mood->label() }}
         </div>
     @endif
-
     {{-- CONTENT --}}
     <div class="whitespace-pre-line text-lg">
         {{ $entry->content }}
     </div>
-
-    {{-- DATE --}}
-    <div class="text-sm text-gray-400">
-        {{ $entry->created_at->format('d M Y H:i') }}
-    </div>
-
 </div>
